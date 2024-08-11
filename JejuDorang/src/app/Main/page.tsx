e2de4498fbd94dc2c2609e5851521fe484df52b4
Hosung99 <shson1217@kw.ac.kr>
2024-08-12 00:38:52 +0900

    ✨ Feat: 경로테스트

--- JejuDorang/src/app/Main/page.tsx
+++ JejuDorang/src/app/Main/page.tsx
@@ -1,8 +1,11 @@
+import Profile from '@/components/Profile/index';
+
 const Main = () => {
   return (
     <>
       <p className="text-main">메인페이지 입니다!</p>
       <h1 className="text-blue">메인페이지 입니다!</h1>
+      <Profile />
     </>
   );
 };
---
+++ JejuDorang/src/components/Profile/index.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react/jsx-runtime';
+// import profileImage from '#/img/profile.webp';
+
+const Profile = () => {
+  return (
+    <Fragment>
+      {/* <img src={profileImage} alt="Profile" />;/ */}
+      <p>Profile</p>
+    </Fragment>
+  );
+};
+
+export default Profile;

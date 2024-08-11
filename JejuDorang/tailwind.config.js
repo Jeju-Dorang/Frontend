8ab04e81b4af1a1545ec5ea33952e354c7a9699e
Hosung99 <shson1217@kw.ac.kr>
2024-08-11 18:38:12 +0900

    ✨ Feat: tailwind에 색상설정추가

--- JejuDorang/tailwind.config.js
+++ JejuDorang/tailwind.config.js
@@ -1,9 +1,17 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#F1C4A3',
+        gray: '#BDBDBD',
+        subgray: '#E8E8E8',
+        black: '#000000',
+        blue: '#5F88FE',
+      },
+    },
   },
   plugins: [],
 };

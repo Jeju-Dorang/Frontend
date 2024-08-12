c5c69f2a009ca1c7b9fcf9cb8df6aaf6d1a51dc7
Hosung99 <shson1217@kw.ac.kr>
2024-08-12 13:32:21 +0900

    ✨ Feat: 프로필컴포넌트 추가

--- JejuDorang/src/app/Main/page.tsx
+++ JejuDorang/src/app/Main/page.tsx
@@ -3,9 +3,7 @@ import Profile from '@components/Profile/index';
 const Main = () => {
   return (
     <>
-      <p className="text-main">메인페이지 입니다!</p>
-      <h1 className="text-blue">메인페이지 입니다!</h1>
-      <Profile />
+      <Profile name="김제주" email="shson1217@naver.com" dDay={27} />
     </>
   );
 };
--- JejuDorang/src/components/Profile/index.tsx
+++ JejuDorang/src/components/Profile/index.tsx
@@ -1,11 +1,24 @@
 import { Fragment } from 'react/jsx-runtime';
-// import profileImage from '#/img/profile.webp';
+import profileImg from '#img/profile.webp';
 
-const Profile = () => {
+interface Props {
+  name: string;
+  email: string;
+  dDay: number;
+}
+
+//이미지도 인자로 받아야함, 백엔드와 얘기필요
+const Profile = ({ name, email, dDay = 0 }: Props) => {
   return (
     <Fragment>
-      {/* <img src={profileImage} alt="Profile" />;/ */}
-      <p>Profile</p>
+      <div className="flex w-full gap-4 p-6">
+        <img src={profileImg} alt="Profile" />
+        <div className="mt-2">
+          <h1 className="font-bold">{name}</h1>
+          <p className="text-gray-lg">{email}</p>
+          <h3 className="decoration-1">🍊제주살이 D-{dDay}</h3>
+        </div>
+      </div>
     </Fragment>
   );
 };

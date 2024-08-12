9b4135712b928088ccf5bc300d65914b048ad947
Hosung99 <shson1217@kw.ac.kr>
2024-08-12 17:39:29 +0900

    ✨ Feat: 내 숙소 주변 둘러보기 컴포넌트

--- JejuDorang/src/components/ArroundKakaoMap/index.tsx
+++ JejuDorang/src/components/ArroundKakaoMap/index.tsx
@@ -1,61 +1,93 @@
+import { CategoryCode } from 'CategoryCodes';
 import { useEffect, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+interface Place {
+  id: string;
+  place_name: string;
+  y: number;
+  x: number;
+}
+
+interface PlacesSearchResultItem {
+  id: string;
+  place_name: string;
+  y: string;
+  x: string;
+}
 
 interface Props {
   lat: number;
   lng: number;
-  category: string;
+  category: CategoryCode;
   css: string;
 }
 
 const ArroundKakaoMap = ({ lat, lng, category, css }: Props) => {
-  const [places, setPlaces] = useState([]);
-  const [map, setMap] = useState(null);
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
+  const [infoWindow, setInfoWindow] = useState<kakao.maps.InfoWindow | null>(
+    null,
+  );
 
   useEffect(() => {
     if (!map) return;
 
-    // console.log(map);
     const ps = new kakao.maps.services.Places();
-    const placesSearchCB = (data, status) => {
+    const placesSearchCB = (
+      result: PlacesSearchResultItem[],
+      status: kakao.maps.services.Status,
+    ) => {
       if (status === kakao.maps.services.Status.OK) {
-        console.log(data);
-        setPlaces(data);
+        const formattedPlaces: Place[] = result.map((item) => ({
+          id: item.id,
+          place_name: item.place_name,
+          y: Number(item.y),
+          x: Number(item.x),
+        }));
+        setPlaces(formattedPlaces);
       } else {
         console.error('장소 검색 실패:', status);
       }
     };
 
-    // 카테고리를 기반으로 장소 검색
     ps.categorySearch(category, placesSearchCB, {
       useMapBounds: true,
       location: new kakao.maps.LatLng(lat, lng),
     });
-  }, [map]);
+  }, [lat, lng, category, map]);
+
+  const handleMarkerClick = (place: Place) => {
+    if (infoWindow) {
+      infoWindow.close();
+      setInfoWindow(null);
+    } else {
+      const newInfoWindow = new kakao.maps.InfoWindow({
+        content: `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`,
+      });
+
+      const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+      if (!map) return;
+
+      newInfoWindow.open(map, marker);
+      setInfoWindow(newInfoWindow);
+    }
+  };
 
   return (
     <div className={`${css}`}>
       <Map
         center={{ lat, lng }}
         style={{ width: '100%', height: '460px' }}
-        onCreate={setMap} // Map 객체를 상태에 저장
+        onCreate={setMap}
       >
         {places.map((place) => (
           <MapMarker
             key={place.id}
             position={{ lat: Number(place.y), lng: Number(place.x) }}
-            onClick={() => {
-              const infoWindow = new kakao.maps.InfoWindow({
-                content: `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`,
-              });
-              // map 상태를 사용하여 인포 윈도우를 엽니다
-              infoWindow.open(
-                map,
-                new kakao.maps.Marker({
-                  position: new kakao.maps.LatLng(place.y, place.x),
-                }),
-              );
-            }}
+            onClick={() => handleMarkerClick(place)}
           />
         ))}
       </Map>

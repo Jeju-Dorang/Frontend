ee1a1a77403f482ffd33cceed96ea47e89509364
lee-youn <5816654@naver.com>
2024-08-13 04:08:07 +0900

    ✨ Feat : 도랑이 프로필 추가

--- JejuDorang/src/app/Main/page.tsx
+++ JejuDorang/src/app/Main/page.tsx
@@ -1,10 +1,23 @@
+import AchievementBox from '@components/AchievementBox';
 import Profile from '@components/Profile/index';
+import StayBox from '@components/StayBox';
 
 const Main = () => {
   return (
     <>
       <Profile name="손성호" email="shson1217@naver.com" dDay={27} />
       <hr />
+      <StayBox
+        name = '카세로지'
+        distance = '530m'
+        location = '제주특별자치도 서귀포시 표선면 가시로 383'
+        description = '표선에 위치한 럭셔리 호텔, 레스토랑 및 바/라운지 이용 가능'
+        />
+      <AchievementBox
+        achievement='흑돼지 킬러'
+        content='제주의 명물, 흑돼지! 이정도 먹었으면 당신은 흑돼지 킬러'
+        title='운동'
+        />
     </>
   );
 };
--- JejuDorang/src/components/StayBox.tsx
+++ JejuDorang/src/components/StayBox.tsx
@@ -14,38 +14,18 @@ const StayBox: React.FC<StayBoxProps> = ({name,distance,location,description}) =
             style={{
                 boxShadow: '0px 1px 6px 0px rgba(0, 0, 0, 0.25)',
             }}>
-            <span className = "absolute top-[17px] left-[25px] text-black"
-            style={{
-                fontFamily : 'Pretendard, sans-serif',
-                fontSize : '15px',
-                fontWeight : '700',
-            }}>
+            <h1 className = "absolute top-[17px] left-[25px] text-black text-[15px] font-bold">
                 {name}
-            </span>
-            <span className = "absolute top-[37px] left-[25px] text-gray-dg"
-            style={{
-                fontFamily : 'Pretendard, sans-serif',
-                fontSize : '10px',
-                fontWeight : '500',
-            }}>
+            </h1>
+            <h2 className = "absolute top-[37px] left-[25px] text-gray-dg text-[10px] font-medium">
                 {distance}
-            </span>
-            <span className = "absolute top-[37px] left-[59px] text-primary-blue"
-            style={{
-                fontFamily : 'Pretendard, sans-serif',
-                fontSize : '10px',
-                fontWeight : '600',
-            }}>
+            </h2>
+            <h2 className = "absolute top-[37px] left-[59px] text-primary-blue text-[10px] font-semibold">
                 {location}
-            </span>
-            <span className = "absolute top-[56px] left-[25px] text-black"
-            style={{
-                fontFamily : 'Pretendard, sans-serif',
-                fontSize : '10px',
-                fontWeight : '600',
-            }}>
+            </h2>
+            <h2 className = "absolute top-[56px] left-[25px] text-black text-[10px] font-semibold">
                 {description}
-            </span>
+            </h2>
             <div className='absolute bottom-[19px] left-[25px] w-[318px] h-[107px] rounded-[10px] gap-[3px] flex bg-gray-dg'>
                 <img src="image-url.jpg" alt="숙소1" className='w-1/2 h-full'/>
                 <img src="image-url.jpg" alt="숙소2" className='w-1/2 h-full'/>

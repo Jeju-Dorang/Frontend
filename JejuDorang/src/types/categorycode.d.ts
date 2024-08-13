91ccfe7ed85ef26b71c420a5becf5b448411f240
Hosung99 <shson1217@kw.ac.kr>
2024-08-13 22:33:50 +0900

    ✨ Feat: 타입 추가

--- JejuDorang/src/types/categorycode.d.ts
+++ JejuDorang/src/types/categorycode.d.ts
@@ -1,7 +1,7 @@
 declare module 'CategoryCodes' {
   export type CategoryCode =
     | 'CS2' // 편의점
-    | 'MT1' //대형마트
-    | 'PM9' //약국
-    | 'BK9'; //은행
+    | 'MT1' // 대형마트
+    | 'PM9' // 약국
+    | 'BK9'; // 은행
 }

246904ce4b2dc7646dc11923708cf45930a8b23e
Hosung99 <shson1217@kw.ac.kr>
2024-08-15 00:30:52 +0900

    ✨ Feat: itemList추가

---
+++ JejuDorang/src/constants/itemList.ts
@@ -0,0 +1,23 @@
+import alpaca from '#img/alpaca.webp';
+import blackPig from '#img/blackPig.webp';
+import boat from '#img/boat.webp';
+import camera from '#img/camera.webp';
+import forest from '#img/forest.webp';
+import orange from '#img/orange.webp';
+import shrimp from '#img/shrimp.webp';
+import teddyBear from '#img/teddyBear.webp';
+import market from '#img/market.webp';
+import museum from '#img/museum.webp';
+
+export const itemList = [
+	{ type: '아이템', url: camera },
+	{ type: '아이템', url: boat },
+	{ type: '아이템', url: orange },
+	{ type: '아이템', url: shrimp },
+	{ type: '아이템', url: teddyBear },
+	{ type: '펫', url: alpaca },
+	{ type: '펫', url: blackPig },
+	{ type: '배경', url: forest },
+	{ type: '배경', url: market },
+	{ type: '배경', url: museum },
+  ];

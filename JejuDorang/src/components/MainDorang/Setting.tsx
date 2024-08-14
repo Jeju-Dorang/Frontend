2ec3214d05b615b3718d6107ae3c11a0c0864cf2
Hosung99 <shson1217@kw.ac.kr>
2024-08-15 00:33:49 +0900

    ✨ Feat: 도랑이 설정하기 버튼 추가

---
+++ JejuDorang/src/components/MainDorang/Setting.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import setting from '#img/setting.webp';
+
+const Setting = () => {
+  return (
+    <Link
+      className="flex justify-end w-full mt-[9px] mr-[13px] mb-[22px] cursor-pointer"
+      to={'/settingDorang'}
+    >
+      <div className="w-[24px] h-[24px]">
+        <img src={setting} alt={'setting'} />
+      </div>
+    </Link>
+  );
+};
+
+export default Setting;

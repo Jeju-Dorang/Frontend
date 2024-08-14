46f75c95312cf5586188f5efd51b572b3c56c0c6
Hosung99 <shson1217@kw.ac.kr>
2024-08-14 17:46:54 +0900

    ✨ Feat: 도랑이 설정 페이지 경로 추가

--- JejuDorang/src/app/App.tsx
+++ JejuDorang/src/app/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from './Main/page';
+import SettingDorang from './SettingDorang/page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/settingDorang" element={<SettingDorang />}></Route>
         {/* <Route path="/dorang" element={<Dorang />}></Route> */}
         {/* <Route path="/activitiy" element={<Activitiy />}></Route> */}
         {/* <Route path="/stay" element={<Stay />}></Route> */}

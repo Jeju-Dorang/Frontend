96c62cca21f37edb9f8612387aaca14d5d089b71
Hosung99 <shson1217@kw.ac.kr>
2024-08-18 22:25:21 +0900

    ♻️ Refactor: border수정

--- JejuDorang/src/components/Location/index.tsx
+++ JejuDorang/src/components/Location/index.tsx
@@ -9,15 +9,18 @@ interface Props {
 const Location = ({ className, selectMap, setSelectMap }: Props) => {
   const renderMap = MAPS.map((item, index) => {
     const isSelected = selectMap === index;
+
     return (
-      <div key={index} onClick={() => setSelectMap(index)}>
-        <img
-          src={item.src}
-          alt={item.name}
-          className={`p-3 border-[3px] rounded-[9px] ${
-            isSelected ? 'border-primary-orange' : 'border-transparent'
-          }`}
-        />
+      <div key={index} className="flex flex-col items-center">
+        <div
+          onClick={() => setSelectMap(index)}
+          className={`w-[75px] h-[75px] rounded-[9px] cursor-pointer border-[3px] ${
+            isSelected ? 'border-primary-orange' : ''
+          } bg-auto`}
+          style={{
+            backgroundImage: `url(${item.src})`,
+          }}
+        ></div>
         <p className="text-[10px] text-center font-semibold">{item.name}</p>
       </div>
     );
@@ -26,7 +29,7 @@ const Location = ({ className, selectMap, setSelectMap }: Props) => {
   return (
     <div className={className}>
       <h3 className="text-[14px] font-semibold mb-[6px] ml-[27px]">위치</h3>
-      <div className="flex flex-row gap-[4px]">{renderMap}</div>
+      <div className="flex flex-row justify-evenly">{renderMap}</div>
     </div>
   );
 };

fac18e7068726aa7b7bf5c80df5c7ed47b2a77ee
Hosung99 <shson1217@kw.ac.kr>
2024-08-18 19:20:39 +0900

    ✨ Feat: map 상수 추가

---
+++ JejuDorang/src/constants/maps.ts
@@ -0,0 +1,11 @@
+import mapEast from '#img/mapEast.webp';
+import mapWest from '#img/mapWest.webp';
+import mapNorth from '#img/mapNorth.webp';
+import mapSouth from '#img/mapSouth.webp';
+
+export const MAPS = [
+  { name: '북', src: mapNorth },
+  { name: '동', src: mapEast },
+  { name: '남', src: mapSouth },
+  { name: '서', src: mapWest },
+];

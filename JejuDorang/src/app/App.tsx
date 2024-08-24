c0c2386f05c45c2e9de861c2180c5d0fe7f03eb9
Hosung99 <shson1217@kw.ac.kr>
2024-08-24 17:51:36 +0900

    ✨ Feat: 로그인 라우팅추가

--- JejuDorang/src/app/App.tsx
+++ JejuDorang/src/app/App.tsx
@@ -3,15 +3,18 @@ import './App.css';
 import Main from './Main/page';
 import SettingDorang from './SettingDorang/page';
 import Activity from './Activity/page';
+import Login from './Login/page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/settingDorang" element={<SettingDorang />}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/kakao/callback" element={<Main />} />
+        <Route path="/settingDorang" element={<SettingDorang />} />
         {/* <Route path="/dorang" element={<Dorang />}></Route> */}
-        <Route path="/activity" element={<Activity />}></Route>
+        <Route path="/activity" element={<Activity />} />
         {/* <Route path="/stay" element={<Stay />}></Route> */}
         {/* <Route path="/mypage" element={<MyPage />}></Route> */}
         {/* <Route path="/record" element={<Record />}></Route> */}

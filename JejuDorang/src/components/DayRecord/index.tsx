af792a6f8ff26868008fc835f6150b919a6c5994
Hosung99 <shson1217@kw.ac.kr>
2024-08-31 22:51:27 +0900

    ✨ Feat: 오늘 일기 쓰기 버튼추가

--- JejuDorang/src/components/DayRecord/index.tsx
+++ JejuDorang/src/components/DayRecord/index.tsx
@@ -45,11 +45,17 @@ const DayRecord = () => {
     console.log('전체보기');
   };
 
+  const writeTodayDiary = () => {
+    console.log('오늘 일기 쓰기');
+  };
+
   return (
     <div>
-      <div className="flex flex-row gap-[12px] mb-[51px]">{renderStory()}</div>
-      <h1 className="text-[14px] mb-[8px] font-semibold">내 일기</h1>
-      <div className="flex w-[280px] mb-[17px] justify-between">
+      <div className="flex flex-row gap-[12px] mb-[51px] ml-[45px]">
+        {renderStory()}
+      </div>
+      <h1 className="text-[14px] mb-[8px] ml-[45px] font-semibold">내 일기</h1>
+      <div className="flex w-[280px] mb-[17px] ml-[45px] justify-between">
         <span className="text-[11px] font-semibold text-gray-lg">
           한달 동안의 추억을 기록해보세요
         </span>
@@ -61,6 +67,14 @@ const DayRecord = () => {
         </button>
       </div>
       <CustomCalendar />
+      <div className="flex justify-center mt-[40px] right-[20px] mb-[30px]">
+        <button
+          className="bg-primary-orange rounded-[3px] px-[56px] py-[14px] font-semibold text-[10px]"
+          onClick={writeTodayDiary}
+        >
+          오늘 일기 쓰기
+        </button>
+      </div>
     </div>
   );
 };

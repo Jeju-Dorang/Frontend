fc1d1a53629e361b8031ac0aab89f22133a46e84
lee-youn <5816654@naver.com>
2024-09-06 19:17:30 +0900

    ⬆️ Chore : 메세지전송 footer 추가

--- JejuDorang/src/components/ChatDetail/Chat/index.tsx
+++ JejuDorang/src/components/ChatDetail/Chat/index.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
 import DorangMessage from "./DorangMessage";
 import UserMessage from "./UserMessage";
+import { CHAT } from "@type/chat";
 
 interface Props {
     interests : string[]
+    messages : CHAT[]
 }
 
-const Chat = ({interests}:Props) => {
-    const [message, setMessage] = useState<string>('');
-    const [messages, setMessages] = useState<string[]>([]);
+const Chat = ({interests, messages}:Props) => {
+    
+    {/* 디버깅코드 */}
+    console.log("messages" , messages)
 
     return (
-
         <div className="w-[100%] h-screen flex flex-col mt-[15px] overflow-y-auto">
             <p className="flex ml-[130px] mt-[10px] text-primary-orange font-semibold text-[12px] items-center">
                 여행키워드 : {interests.join(', ')}
@@ -21,9 +22,14 @@ const Chat = ({interests}:Props) => {
                     message="안녕하세요 챗봇 도랑이입니다.
 어떤 여행지를 추천해드릴까요?"
                 />
-                <UserMessage
-                    message="안녕 관광지 추천해줘."
-                />
+
+                {messages.map((msg, index) => (
+                    msg.type === 'user' ? 
+                        <UserMessage key={index} message={msg.text} /> :
+                        <DorangMessage key={index} message={msg.text}/>
+                        // url={msg.url} 나중에 도랑이 메시지에 url도 넣을거임
+                    ))}
+                
             </div>
         </div>
 
--- JejuDorang/src/components/ChatDetail/index.tsx
+++ JejuDorang/src/components/ChatDetail/index.tsx
@@ -4,12 +4,24 @@ import Onboarding from "./Onboarding";
 import InputMessage from "./Chat/InputMessage";
 import ChatHeader from "@components/ChatHeader";
 import Footer from "@components/Footer";
+import { CHAT } from "@type/chat";
 
 
 const ChatDetail = () => {
     const [openOnboarding, setOpenOnboarding] = useState<boolean>(true);
     const [interests, setInterests] = useState<string[]>([]);
     const [showFooter, setShowFooter] = useState(false);
+    const [messages, setMessages] = useState<CHAT[]>([]);
+
+    const sendMessage = (message:string) => {
+        handleChatMessage({ type: 'user', text: message }); 
+    };
+
+    const handleChatMessage = (message:{ type: "bot" | "user"; text: string; url?:string }) => {
+        setMessages((prevState) => {
+            return [...prevState, message];
+        });
+    }
 
     const handleCloseOnboarding = (savedInterests:string[]) => {
         setOpenOnboarding(!openOnboarding)
@@ -46,10 +58,10 @@ const ChatDetail = () => {
         <div className="flex flex-col h-screen w-[100%] bg-gray-100">
         {openOnboarding ?
             <Onboarding openOnboarding={handleCloseOnboarding}/> :
-            <Chat interests={interests} />
+            <Chat interests={interests} messages={messages} />
         }
         {(!openOnboarding && !showFooter) ? 
-            <InputMessage /> : <Footer />}
+            <InputMessage sendMessage = {sendMessage} /> : <Footer />}
         
         </div>
         </>

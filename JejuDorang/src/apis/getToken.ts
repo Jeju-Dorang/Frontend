1c49b9a86d62afbf90117a27470f5aba13441c95
Hosung99 <shson1217@kw.ac.kr>
2024-09-10 21:57:24 +0900

    🚀 Test: 로그인 헤더로받도록 수정

--- JejuDorang/src/apis/getToken.ts
+++ JejuDorang/src/apis/getToken.ts
@@ -1,19 +1,17 @@
 import { api } from './index';
 import { useAuthStore } from '@states/useAuthStore';
 
-interface KakaoLoginResponse {
-  token: string;
-}
-
 export const getToken = async (code: string): Promise<boolean> => {
   return api
-    .get<KakaoLoginResponse>(false, `/auth/kakao/login?code=${code}`)
+    .get<void>(false, `/auth/kakao/login?code=${code}`)
     .then((response) => {
-      if (response.data && response.data.token) {
-        useAuthStore.getState().setToken(response.data.token);
+      const token = response.headers['Authorization'];
+      console.log(token);
+      if (token) {
+        useAuthStore.getState().setToken(token);
         return true;
       } else {
-        console.error('Token not found in the response');
+        console.error('Token not found in the response headers');
         return false;
       }
     })

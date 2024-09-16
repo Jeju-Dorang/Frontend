f64b7af4e47c009c062c8e319f5430c466801ad0
Hosung99 <shson1217@kw.ac.kr>
2024-09-16 22:48:14 +0900

    ✨ Feat: 기록페이지 api추가

--- JejuDorang/src/apis/community.ts
+++ JejuDorang/src/apis/community.ts
@@ -1,13 +1,89 @@
 import { api } from './index';
+import { Question, DetailQuestion } from '@type/question';
 
-export const searchPost = async (searchString: string) => {
+// const searchPost = async (searchString: string) => {
+//   try {
+//     // const response = await api.get<void>(
+//     //   false,
+//     //   ``,
+//     // );
+//   } catch (error) {
+//     console.error('Kakao login failed:', error);
+//     return;
+//   }
+// };
+
+const getQuestions = async (): Promise<Question[] | null> => {
   try {
-    // const response = await api.get<void>(
-    //   false,
-    //   ``,
-    // );
+    const response = await api.get<Question[]>(true, `/posts/questions`);
+    return response.data;
   } catch (error) {
-    console.error('Kakao login failed:', error);
-    return;
+    console.error('Failed to post question:', error);
+    return null;
   }
+};
+
+const getQuestion = async (postId: number): Promise<DetailQuestion | null> => {
+  try {
+    const response = await api.get<DetailQuestion>(
+      true,
+      `/posts/question/${postId}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Failed to get question:', error);
+    return null;
+  }
+};
+
+const postCommentLike = async (commentId: number): Promise<boolean> => {
+  try {
+    await api.post<boolean, null>(
+      true,
+      `/posts/like/comment/${commentId}`,
+      null,
+    );
+    return true;
+  } catch (error) {
+    console.error('Failed to like comment:', error);
+    return false;
+  }
+};
+
+const postQuestion = async (
+  title: string,
+  content: string,
+): Promise<boolean> => {
+  try {
+    await api.post<void, { title: string; content: string }>(
+      true,
+      `/posts/question`,
+      { title, content },
+    );
+    return true;
+  } catch (error) {
+    console.error('Failed to post question:', error);
+    return false;
+  }
+};
+
+const postComment = async (
+  postId: number,
+  content: string,
+): Promise<boolean> => {
+  try {
+    await api.post<void, string>(true, `/posts/comment/${postId}`, content);
+    return true;
+  } catch (error) {
+    console.error('Failed to post question:', error);
+    return false;
+  }
+};
+
+export {
+  getQuestions,
+  getQuestion,
+  postQuestion,
+  postComment,
+  postCommentLike,
 };
--- JejuDorang/src/apis/diary.ts
+++ JejuDorang/src/apis/diary.ts
@@ -1,28 +1,25 @@
 import { api } from './index';
-import { StoryItem } from '@type/storyItem';
+import { DetailStory, StoryItem } from '@type/storyItem';
 import { Diary } from '@type/diary';
 import { Streak } from '@type/streak';
 
 const getStories = async (): Promise<StoryItem[] | null> => {
   try {
-    const res = await api.get<StoryItem[]>(true, '/posts/diaries');
-    if (res.status === 200) {
-      return res.data;
-    }
-    return null;
+    const response = await api.get<StoryItem[]>(true, '/posts/diaries');
+    return response.data;
   } catch (error) {
     console.error('Error fetching diary:', error);
     return null;
   }
 };
 
-const getStory = async (diaryId: number): Promise<Diary | null> => {
+const getStory = async (diaryId: number): Promise<DetailStory | null> => {
   try {
-    const res = await api.get<Diary>(true, `/posts/diaries/${diaryId}`);
-    if (res.status === 200) {
-      return res.data;
-    }
-    return null;
+    const response = await api.get<DetailStory>(
+      true,
+      `/posts/diaries/${diaryId}`,
+    );
+    return response.data;
   } catch (error) {
     console.error('Error fetching diary:', error);
     return null;
@@ -34,28 +31,35 @@ const getStreaks = async (
   month: number,
 ): Promise<Streak[] | null> => {
   try {
-    const res = await api.get<Streak[]>(
+    const response = await api.get<Streak[]>(
       true,
       `/posts/streaks?year=${year}&month=${month}`,
     );
-    if (res.status === 200) {
-      return res.data;
-    }
-    return null;
+    return response.data;
   } catch (error) {
     console.error('Error fetching streaks:', error);
     return null;
   }
 };
 
-const postDiary = async (diary: Diary) => {
+const postStoryLike = async (diaryId: number): Promise<boolean> => {
   try {
-    await api.post<boolean, Diary>(true, `/posts/diary`, diary);
+    await api.post<boolean, null>(true, `/posts/like/diary/${diaryId}`, null);
     return true;
   } catch (error) {
-    console.error('Failed to fetch stories:', error);
+    console.error('Failed to like diary:', error);
     return false;
   }
 };
 
-export { getStories, getStory, postDiary, getStreaks };
+const postDiary = async (diary: Diary): Promise<boolean> => {
+  try {
+    await api.post<boolean, Diary>(true, `/posts/diary`, diary);
+    return true;
+  } catch (error) {
+    console.error('Failed to post diary:', error);
+    return false;
+  }
+};
+
+export { getStories, getStory, getStreaks, postDiary, postStoryLike };

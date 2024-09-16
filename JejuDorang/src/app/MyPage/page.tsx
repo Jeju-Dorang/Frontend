0848a3516d2e389ca16a0187f3a7177ccd016cff
lee-youn <5816654@naver.com>
2024-09-17 01:51:53 +0900

    ✨ Feat : 메인마이페이지 일기, 업적 추가

--- JejuDorang/src/app/MyPage/page.tsx
+++ JejuDorang/src/app/MyPage/page.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
 import MyPageProfile from '@components/MyPageProfile';
+import MyPageDiary from '@components/MyPageDiary';
+import MyPageAchievement from '@components/MyPageAchievement';
 
 const MyPage = () => {
     useEffect(() => {}, []);
     return (
         <>
         <MyPageProfile />
+        <MyPageDiary />
+        <MyPageAchievement />
         </>
     );
 };

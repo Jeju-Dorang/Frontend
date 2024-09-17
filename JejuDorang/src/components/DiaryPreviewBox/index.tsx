6f95e80d03f9d316a4a15cc952edfa5421e8244f
Hosung99 <shson1217@kw.ac.kr>
2024-09-18 02:14:16 +0900

    🔥 Refactor: 배포용 Jsont 수정

--- JejuDorang/src/components/DiaryPreviewBox/index.tsx
+++ JejuDorang/src/components/DiaryPreviewBox/index.tsx
@@ -1,29 +1,32 @@
-import { Diary } from "@type/diary";
+import { Diary } from '@type/diary';
 
-
-const DiaryPreviewBox = ({title, content, status} : Diary) => {
-    return(
-        <button className="flex flex-col h-[75px] w-80 border border-gray-dg rounded-[10px]
-                        items-start justify-center">
-            <div className="flex flex-row ml-5 mt-4">
-                <h2 className="text-[12px] font-semibold text-black">
-                    제목 : {title}
-                </h2>
-                <p className={`ml-3 text-[11px] font-semibold 
-                                ${status === 'Private' ? 
-                                'text-gray-dg' : 
-                                'text-primary-blue'}`}>
-                    {status}
-                </p>
-            </div>
-            <h3 className="flex-grow w-72 text-[12px] font-normal text-black ml-5 mt-1 mr-5
-                            whitespace-nowrap overflow-hidden text-ellipsis">
-                내용 : {content}
-            </h3>
-
-        </button>
-
-    );
-}
+const DiaryPreviewBox = ({ title, content, secret }: Diary) => {
+  return (
+    <button
+      className="flex flex-col h-[75px] w-80 border border-gray-dg rounded-[10px]
+                        items-start justify-center"
+    >
+      <div className="flex flex-row ml-5 mt-4">
+        <h2 className="text-[12px] font-semibold text-black">제목 : {title}</h2>
+        <p
+          className={`ml-3 text-[11px] font-semibold
+                                ${
+                                  secret === 'Private'
+                                    ? 'text-gray-dg'
+                                    : 'text-primary-blue'
+                                }`}
+        >
+          {secret}
+        </p>
+      </div>
+      <h3
+        className="flex-grow w-72 text-[12px] font-normal text-black ml-5 mt-1 mr-5
+                            whitespace-nowrap overflow-hidden text-ellipsis"
+      >
+        내용 : {content}
+      </h3>
+    </button>
+  );
+};
 
 export default DiaryPreviewBox;

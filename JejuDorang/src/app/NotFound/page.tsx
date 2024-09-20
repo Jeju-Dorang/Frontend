4a542701ed460dcab5084e9059959c65057da347
Hosung99 <shson1217@kw.ac.kr>
2024-09-20 21:36:08 +0900

    ♻️ Refactor: 에러페이지 이미지 크기 수정

--- JejuDorang/src/app/NotFound/page.tsx
+++ JejuDorang/src/app/NotFound/page.tsx
@@ -1,13 +1,20 @@
 import errorPage from '#img/errorPage.webp';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-white">
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-white">
       <img
         src={errorPage}
-        className="w-full h-auto max-h-[calc(100vh-40px)]"
+        className="w-full h-auto max-h-[calc(100vh-120px)] object-cover"
         alt="404 Error"
       />
+      <Link
+        to="/"
+        className="mt-6 px-6 py-3 bg-blue-500 font-semibold rounded-full shadow-md hover:bg-blue-600 transition duration-300 ease-in-out transform hover:-translate-y-1"
+      >
+        Go To Main
+      </Link>
       <a
         href="http://www.freepik.com"
         className="mt-4 text-center text-sm text-gray-500 hover:text-gray-700 transition-colors"

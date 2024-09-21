650b25e197be2f22da8c832e66c6e366c62acee6
Hosung99 <shson1217@kw.ac.kr>
2024-09-21 20:06:49 +0900

    ✨ Feat: 도랑이 세팅에서 기본 설정 추가

--- JejuDorang/src/components/ItemBox/index.tsx
+++ JejuDorang/src/components/ItemBox/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ITEM_LIST } from '@constants/itemList';
 import { DORANG_CATEGORY } from '@constants/category';
 
@@ -19,10 +19,24 @@ const ItemBox = ({
   setBackgroundImageUrl,
 }: Props) => {
   const [category, setCategory] = useState<string>('아이템');
+  const [selectedItems, setSelectedItems] = useState({
+    아이템: 'default',
+    펫: 'default',
+    배경: 'default',
+  });
+
+  useEffect(() => {
+    // 초기 상태 설정
+    setItemImageUrl('');
+    setPetImageUrl('');
+    setBackgroundImageUrl('');
+  }, []);
 
   const filteredItems = ITEM_LIST.filter((item) => item.type === category);
 
   const handleItemClick = (url: string | null) => {
+    const newUrl = url || 'default';
+    setSelectedItems((prev) => ({ ...prev, [category]: newUrl }));
     switch (category) {
       case '아이템':
         setItemImageUrl(url || '');
@@ -53,16 +67,25 @@ const ItemBox = ({
       </div>
       <div className="flex flex-wrap justify-start gap-[10px] px-[10px]">
         <div
-          className="flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md"
+          className={`flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md ${
+            selectedItems[category as keyof typeof selectedItems] === 'default'
+              ? 'bg-gray-300'
+              : ''
+          }`}
           onClick={() => handleItemClick(null)}
         ></div>
         {filteredItems.map((item) => (
           <div
             key={item.url}
-            className="flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md"
+            className={`relative flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md ${
+              selectedItems[category as keyof typeof selectedItems] === item.url
+                ? 'bg-gray-300'
+                : ''
+            }`}
             onClick={() => handleItemClick(item.url)}
           >
             <img src={item.url} alt="item" className="w-[40px] h-[40px]" />
+            {selectedItems[category as keyof typeof selectedItems] === item.url}
           </div>
         ))}
       </div>

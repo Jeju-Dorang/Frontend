9b922dac639850822fa8155ca3b99fecf85d084d
Hosung99 <shson1217@kw.ac.kr>
2024-09-25 16:51:26 +0900

    🐛 Fix: BrowserRouter중복 에러 해결

--- JejuDorang/src/app/App.tsx
+++ JejuDorang/src/app/App.tsx
@@ -55,100 +55,98 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div className="flex justify-center items-center w-full min-h-screen bg-background">
-        <div className="w-full h-full min-h-screen max-w-[402px] bg-white shadow-[0_0_10px_rgba(0,0,0,0.1)] custom:rounded-[20px] flex flex-col">
-          <div className="flex-grow overflow-y-auto ">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <Main />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/settingDorang"
-                element={
-                  <PrivateRoute>
-                    <SettingDorang />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/dorang"
-                element={
-                  <PrivateRoute>
-                    <Dorang setIsNavVisible={setIsNavVisible} />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/activity"
-                element={
-                  <PrivateRoute>
-                    <Activity />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/stay"
-                element={
-                  <PrivateRoute>
-                    <Stay />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/record"
-                element={
-                  <PrivateRoute>
-                    <Record />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/mypage"
-                element={
-                  <PrivateRoute>
-                    <MyPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/mypage/edit"
-                element={
-                  <PrivateRoute>
-                    <EditMyPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/allDiaries"
-                element={
-                  <PrivateRoute>
-                    <AllDiaries />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/stay/recommend"
-                element={
-                  <PrivateRoute>
-                    <RecommendStay />
-                  </PrivateRoute>
-                }
-              />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-          {isNavVisible && <Footer currentPage={currentPage} />}
+    <div className="flex justify-center items-center w-full min-h-screen bg-background">
+      <div className="w-full h-full min-h-screen max-w-[402px] bg-white shadow-[0_0_10px_rgba(0,0,0,0.1)] custom:rounded-[20px] flex flex-col">
+        <div className="flex-grow overflow-y-auto ">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Main />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/settingDorang"
+              element={
+                <PrivateRoute>
+                  <SettingDorang />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/dorang"
+              element={
+                <PrivateRoute>
+                  <Dorang setIsNavVisible={setIsNavVisible} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/activity"
+              element={
+                <PrivateRoute>
+                  <Activity />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/stay"
+              element={
+                <PrivateRoute>
+                  <Stay />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/record"
+              element={
+                <PrivateRoute>
+                  <Record />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/mypage"
+              element={
+                <PrivateRoute>
+                  <MyPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/mypage/edit"
+              element={
+                <PrivateRoute>
+                  <EditMyPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/allDiaries"
+              element={
+                <PrivateRoute>
+                  <AllDiaries />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/stay/recommend"
+              element={
+                <PrivateRoute>
+                  <RecommendStay />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
+        {isNavVisible && <Footer currentPage={currentPage} />}
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 

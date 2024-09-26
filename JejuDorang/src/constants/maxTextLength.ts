306d9f48da31ddbb92ea58e424a5659a953e66b4
lee-youn <5816654@naver.com>
2024-09-26 10:41:19 +0900

    ⬆️ Chore : 브랜치 충돌

--- JejuDorang/src/constants/maxTextLength.ts
+++ JejuDorang/src/constants/maxTextLength.ts
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 export const MAX_DIARY_TITLE_LENGTH = 20;
 export const MAX_DIARY_CONTENT_LENGTH = 60;
 export const MAX_POST_TITLE_LENGTH = 30;
 export const MAX_POST_COMMENT_LENGTH = 1000;
 export const MAX_PROFILE_DETAIL_LENGTH = 20;
+=======
+export const MAX_TEXT_LENGTH = 60;
+export const MAX_TITLE_LENGTH = 30;
+export const MAX_LIST_LENGTH = 1000;
+export const MAX_DIARY_TITLE_LENGTH = 20;
+>>>>>>> fb91f84 (✨ Feat: 숙소추천페이지 완성)

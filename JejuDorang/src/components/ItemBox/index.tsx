a6ae76695660693534109897f8a5f5b27e973864
Hosung99 <shson1217@kw.ac.kr>
2024-09-27 15:00:12 +0900

    ✨ Feat: 도랑이 아이템 장착 api적용

--- JejuDorang/src/components/ItemBox/index.tsx
+++ JejuDorang/src/components/ItemBox/index.tsx
@@ -1,54 +1,91 @@
 import { useState, useEffect } from 'react';
 import { ITEM_LIST } from '@constants/itemList';
 import { DORANG_CATEGORY } from '@constants/category';
+import { getDorangItems } from '@apis/main';
+import { dorangItem } from '@type/dorangItem';
 
 const buttonStyles = (isActive: boolean) =>
   `w-[64px] h-[31px] text-[15px] leading-[140%] rounded-[50px] border whitespace-nowrap hover:bg-primary-orange ${
     isActive ? 'bg-primary-orange text-white' : 'bg-white text-[#515356]'
   }`;
-
 interface Props {
-  setItemImageUrl: (value: string) => void;
-  setPetImageUrl: (value: string) => void;
-  setBackgroundImageUrl: (value: string) => void;
+  setItemImageIndex: (value: number) => void;
+  setPetImageIndex: (value: number) => void;
+  setBackgroundImageIndex: (value: number) => void;
 }
 
 const ItemBox = ({
-  setItemImageUrl,
-  setPetImageUrl,
-  setBackgroundImageUrl,
+  setItemImageIndex,
+  setPetImageIndex,
+  setBackgroundImageIndex,
 }: Props) => {
   const [category, setCategory] = useState<string>('아이템');
   const [selectedItems, setSelectedItems] = useState({
-    아이템: 'default',
-    펫: 'default',
-    배경: 'default',
+    아이템: 0,
+    펫: 0,
+    배경: 0,
+  });
+  const [userItems, setUserItems] = useState<dorangItem>({
+    backgroundImage: [],
+    itemImage: [],
+    petImage: [],
   });
 
+  const fetchItems = async () => {
+    try {
+      const data = await getDorangItems();
+      setUserItems({
+        backgroundImage: data?.backgroundImage || [],
+        itemImage: data?.itemImage || [],
+        petImage: data?.petImage || [],
+      });
+    } catch (error) {
+      console.error("Failed to fetch user's items:", error);
+    }
+  };
   useEffect(() => {
-    // 초기 상태 설정
-    setItemImageUrl('');
-    setPetImageUrl('');
-    setBackgroundImageUrl('');
+    fetchItems();
   }, []);
 
-  const filteredItems = ITEM_LIST.filter((item) => item.type === category);
+  const filteredItems = ITEM_LIST[category as keyof typeof ITEM_LIST];
 
-  const handleItemClick = (url: string | null) => {
-    const newUrl = url || 'default';
-    setSelectedItems((prev) => ({ ...prev, [category]: newUrl }));
+  const isItemOwned = (index: number): boolean => {
     switch (category) {
       case '아이템':
-        setItemImageUrl(url || '');
-        break;
+        return (
+          index === 0 ||
+          userItems.itemImage.some((item) => item.index === index)
+        );
       case '펫':
-        setPetImageUrl(url || '');
-        break;
+        return (
+          index === 0 || userItems.petImage.some((item) => item.index === index)
+        );
       case '배경':
-        setBackgroundImageUrl(url || '');
-        break;
+        return (
+          index === 0 ||
+          userItems.backgroundImage.some((item) => item.index === index)
+        );
       default:
-        break;
+        return false;
+    }
+  };
+
+  const handleItemClick = (index: number) => {
+    if (isItemOwned(index)) {
+      setSelectedItems((prev) => ({ ...prev, [category]: index }));
+      switch (category) {
+        case '아이템':
+          setItemImageIndex(index);
+          break;
+        case '펫':
+          setPetImageIndex(index);
+          break;
+        case '배경':
+          setBackgroundImageIndex(index);
+          break;
+        default:
+          break;
+      }
     }
   };
 
@@ -66,28 +103,32 @@ const ItemBox = ({
         ))}
       </div>
       <div className="flex flex-wrap justify-start gap-[10px] px-[10px]">
-        <div
-          className={`flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md ${
-            selectedItems[category as keyof typeof selectedItems] === 'default'
-              ? 'bg-gray-300'
-              : ''
-          }`}
-          onClick={() => handleItemClick(null)}
-        ></div>
-        {filteredItems.map((item) => (
-          <div
-            key={item.url}
-            className={`relative flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer hover:bg-primary-orange hover:shadow-md ${
-              selectedItems[category as keyof typeof selectedItems] === item.url
-                ? 'bg-gray-300'
-                : ''
-            }`}
-            onClick={() => handleItemClick(item.url)}
-          >
-            <img src={item.url} alt="item" className="w-[40px] h-[40px]" />
-            {selectedItems[category as keyof typeof selectedItems] === item.url}
-          </div>
-        ))}
+        {Object.entries(filteredItems).map(([index, item]) => {
+          const itemIndex = parseInt(index);
+          const isSelected =
+            selectedItems[category as keyof typeof selectedItems] === itemIndex;
+          const isOwned = isItemOwned(itemIndex);
+          return (
+            <div
+              key={item.url}
+              className={`relative flex flex-row items-center w-[64px] h-[60px] border-2 rounded-[16px] justify-center cursor-pointer
+                  ${
+                    isOwned
+                      ? 'hover:bg-primary-orange hover:shadow-md'
+                      : 'opacity-50 cursor-not-allowed'
+                  }
+                  ${isSelected ? 'bg-gray-300' : ''}`}
+              onClick={() => isOwned && handleItemClick(itemIndex)}
+            >
+              {item.url && (
+                <img src={item.url} alt="item" className="w-[40px] h-[40px]" />
+              )}
+              {isSelected && (
+                <div className="absolute top-0 right-0 w-3 h-3 bg-primary-orange rounded-full" />
+              )}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- JejuDorang/src/components/MainDorang/index.tsx
+++ JejuDorang/src/components/MainDorang/index.tsx
@@ -5,21 +5,24 @@ import Character from './Character/index';
 import DorangChat from './DorangChat/index';
 import LinkToDorang from './LinkToDorang/index';
 import CompleteAchievement from './Achievement/index';
+import { ITEM_LIST } from '@constants/itemList';
 
 const MainDorang = () => {
-  const [itemImageUrl, setItemImageUrl] = useState<string | null>(null);
-  const [petImageUrl, setPetImageUrl] = useState<string | null>(null);
-  const [backgroundImageUrl, setBackgroundImageUrl] = useState<string | null>(
-    null,
-  );
+  const [itemImageUrl, setItemImageUrl] = useState<string>('');
+  const [petImageUrl, setPetImageUrl] = useState<string>('');
+  const [backgroundImageUrl, setBackgroundImageUrl] = useState<string>('');
+
   useEffect(() => {
-    const imageUrl = useAuthStore.getState().characterImage;
-    if (imageUrl !== null) {
-      setItemImageUrl(imageUrl.itemImage);
-      setPetImageUrl(imageUrl.petImage);
-      setBackgroundImageUrl(imageUrl.backGroundImage);
+    const characterImage = useAuthStore.getState().characterImage;
+    if (characterImage !== null) {
+      setItemImageUrl(ITEM_LIST.아이템[characterImage.itemImage]?.url || '');
+      setPetImageUrl(ITEM_LIST.펫[characterImage.petImage]?.url || '');
+      setBackgroundImageUrl(
+        ITEM_LIST.배경[characterImage.backgroundImage]?.url || '',
+      );
     }
   }, []);
+
   return (
     <div className="flex flex-col h-fit">
       <div

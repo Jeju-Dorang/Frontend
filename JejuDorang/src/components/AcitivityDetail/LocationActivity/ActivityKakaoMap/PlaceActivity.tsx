22a4ed30b358335bf3b1861ed7de4c497d84e091
lee-youn <5816654@naver.com>
2024-09-30 00:03:37 +0900

    ✨ Feat : 위치 기반 놀거리 추천 새창 이동 추가

--- JejuDorang/src/components/AcitivityDetail/LocationActivity/ActivityKakaoMap/PlaceActivity.tsx
+++ JejuDorang/src/components/AcitivityDetail/LocationActivity/ActivityKakaoMap/PlaceActivity.tsx
@@ -26,59 +26,60 @@ const PlaceActivity = ({
     const handleMarkerClick = (place: StayApiResponse) => {
         const markerPosition = new kakao.maps.LatLng(parseFloat(place.mapY), parseFloat(place.mapX));
         if (!map) return;
-        
+
         const findRoute = () => {
-        window.open(
-            `https://map.kakao.com/link/to/${place.title},${place.mapY},${place.mapX}/from/현재 위치,${myLat},${myLng}`,
-        );};
+            window.open(
+                `https://map.kakao.com/link/to/${place.title},${place.mapY},${place.mapX}/from/현재 위치,${myLat},${myLng}`,
+            );
+        };
 
-    const newInfoWindow = new kakao.maps.InfoWindow({
-        content: `
-        <div class="p-1 font-[12px] text-center min-w-[150px]">
-            <div>${place.title}</div>
-            <button 
-                id="routeButton" 
-                class="mt-1 p-1 bg-primary-orange text-white 
-                border-none rounded cursor-pointer w-full">
-                길찾기
-            </button>
-        </div>
-        `,
-    });
-    if (infoWindow) {
-        infoWindow.close();
-        setInfoWindow(null);
-    }
-    if (!infoWindow || infoWindow.getContent() !== newInfoWindow.getContent()) {
-        newInfoWindow.open(
-            map,
-            new kakao.maps.Marker({
-                position: markerPosition,
-        }),
-        );
-        setInfoWindow(newInfoWindow);
+        const newInfoWindow = new kakao.maps.InfoWindow({
+            content: `
+            <div class="p-1 font-[12px] text-center min-w-[150px]">
+                <div>${place.title}</div>
+                <button id="routeButton" 
+                        class="mt-1 p-1 bg-primary-orange text-white border-none rounded cursor-pointer w-full">
+                    길찾기
+                </button>
+            </div>
+            `,
+        });
 
-        setTimeout(() => {
+        // 현재 열려 있는 InfoWindow 닫기
+        if (infoWindow) {
+            infoWindow.close();
+            setInfoWindow(null);
+        }
+
+        // 새로운 InfoWindow 열기
+        if (!infoWindow || infoWindow.getContent() !== newInfoWindow.getContent()) {
+            newInfoWindow.open(
+                map,
+                new kakao.maps.Marker({
+                    position: markerPosition,
+                }),
+            );
+            setInfoWindow(newInfoWindow);
+            
             const routeButton = document.getElementById('routeButton');
-            if (routeButton) {
-                routeButton.addEventListener('click', findRoute);
-            }
-        }, 100);
-    } else {
-        setInfoWindow(null);
-    }
+                if (routeButton) {
+                    routeButton.addEventListener('click', findRoute);
+                }
+        } else {
+            setInfoWindow(null);
+        }
     };
 
     return (
-    <Fragment>
-        {places.map((place, index) => (
-        <MapMarker
-            key={index}
-            position={{ lat: Number(place.mapY), lng: Number(place.mapX) }}
-            onClick={() => handleMarkerClick(place)}
-        />
-        ))}
-    </Fragment>
+        <Fragment>
+            {places.map((place, index) => (
+                <MapMarker
+                    key={index}
+                    position={{ lat: Number(place.mapY), lng: Number(place.mapX) }}
+                    onClick={() => handleMarkerClick(place)}
+                />
+            ))}
+        </Fragment>
     );
 };
 
--- JejuDorang/src/components/AcitivityDetail/LocationActivity/ActivityKakaoMap/index.tsx
+++ JejuDorang/src/components/AcitivityDetail/LocationActivity/ActivityKakaoMap/index.tsx
@@ -16,53 +16,10 @@ interface Props {
 
 
 const ActivityKakaoMap = ({ lat, lng, css, activityData }: Props) => {
-    const category:CategoryCode = 'AT4';
     const [places, setPlaces] = useState<Place[]>([]);
     const [map, setMap] = useState<kakao.maps.Map | null>(null);
     const [markers, setMarkers] = useState<kakao.maps.Marker[]>([]);
     const [infoWindow, setInfoWindow] = useState<kakao.maps.InfoWindow | null>(null,);
-
-    useEffect(() => {
-        if (!map) return;
-    
-        markers.forEach((marker) => marker.setMap(null));
-        setMarkers([]);
-        if (infoWindow) {
-            infoWindow.close();
-            setInfoWindow(null);
-        }
-    
-        const ps = new kakao.maps.services.Places();
-        const placesSearchCB = (
-            result: PlacesSearchResultItem[],
-            status: kakao.maps.services.Status,
-        ) => {
-            if (status === kakao.maps.services.Status.OK) {
-                const formattedPlaces: Place[] = result.map((item) => ({
-                id: item.id,
-                place_name: item.place_name,
-                y: Number(item.y),
-                x: Number(item.x),
-                }));
-                setPlaces(formattedPlaces);
-                const newMarkers = formattedPlaces.map((place) => {
-                const marker = new kakao.maps.Marker({
-                    position: new kakao.maps.LatLng(place.y, place.x),
-                    map: map,
-                });
-                return marker;
-                });
-                setMarkers(newMarkers);
-            } else {
-                console.error('장소 검색 실패:', status);
-            }
-            };
-    
-    ps.categorySearch(category, placesSearchCB, {
-        useMapBounds: true,
-        location: new kakao.maps.LatLng(lat, lng),
-    });
-    }, [lat, lng, category, map]);
 
     return (
         <div className={`${css}`}>
@@ -79,17 +36,6 @@ const ActivityKakaoMap = ({ lat, lng, css, activityData }: Props) => {
                     width: 50,
                     height: 50
                 }}}/>
-{/* 
-                {activityData.map((activity, index) => (
-                    <MapMarker
-                    key={index}
-                    position={{
-                        lat: Number(activity.mapY),
-                        lng: Number(activity.mapX),
-                    }}>
-                        <div className="p-1 text-black">{activity.title}</div>
-                    </MapMarker>
-                ))} */}
                 
                 <PlaceActivity
                     places={activityData}

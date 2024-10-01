06c0a4a16b72506ecc09181ec86ce63f9f7e0d08
lee-youn <5816654@naver.com>
2024-10-01 10:24:05 +0900

    ✨ Feat : 일기 전체보기 일기 없을 때 추가

--- JejuDorang/src/app/AllDiaries/page.tsx
+++ JejuDorang/src/app/AllDiaries/page.tsx
@@ -1,10 +1,10 @@
 import { getAllDiaries } from "@apis/allDiaries";
 import DiaryPreviewBox from "@components/DiaryPreviewBox";
 import ViewDiary from "@components/ViewDiary";
-import { Diary } from "@type/diary";
 import { DiaryPreview } from "@type/diaryPreview";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DorangProfile from "#img/dorangProfile.webp";
 
 const AllDiaries = () => {
     const [diaryList, setDiaryList] = useState<DiaryPreview[]>([]);
@@ -31,6 +31,7 @@ const AllDiaries = () => {
         }
     };
 
+    console.log("diarylist : ", diaryList);
 
     return(
         <div className="flex flex-col w-full h-screen mt-10">
@@ -44,6 +45,24 @@ const AllDiaries = () => {
                     마이페이지로 가기
                 </button>
             </div>
+            {diaryList.length === 0 ?
+            <div className="flex flex-col justify-center items-center mx-10 mt-16">
+                <div className="flex flex-col bg-gray-lg rounded-[20px] h-96 w-full mx-10 items-center justify-center gap-3">
+                    <img
+                        src ={DorangProfile}
+                        className="w-20 h-20"
+                    />
+                    <p className="text-darkest-gray font-semibold text-[17px] text-center">
+                        작성한 일기가 없어요<br/>
+                        일기 쓰러 가볼까요?
+                    </p>
+                    <button onClick={() => navigate('/record')}
+                            className="w-[80%] h-10 rounded-[10px] text-[15px] bg-primary-orange text-white font-semibold hover:text-black">
+                                일기 작성하러 가기
+                    </button>
+                </div>
+            </div>
+            :
             <div className="flex flex-col justify-center items-center gap-2 mt-7">
                 {diaryList.map((diary, index) => (
                     <DiaryPreviewBox
@@ -56,6 +75,7 @@ const AllDiaries = () => {
                     />
                 ))}
             </div>
+            }
             {isViewDiary && 
                 <ViewDiary 
                     diaryId = {isviewDiaryId}

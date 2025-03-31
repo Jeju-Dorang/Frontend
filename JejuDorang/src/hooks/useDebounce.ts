5b3f27ea18f47fbe778eeb32976129fa2457ad03
Hosung99 <shson1217@kw.ac.kr>
2025-03-31 19:14:10 +0900

    refactor: 디바운싱 훅 간편하게 수정

--- JejuDorang/src/hooks/useDebounce.ts
+++ JejuDorang/src/hooks/useDebounce.ts
@@ -1,23 +1,19 @@
 import { useCallback, useRef } from 'react';
 
-interface DebouncedFunction {
-  (...args: number[]): Promise<void> | void;
-}
-
-export function useDebounce<T extends DebouncedFunction>(
+export function useDebounce<T extends (args: number) => unknown>(
   callback: T,
   delay: number,
 ): (...args: Parameters<T>) => void {
   const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
   return useCallback(
-    (...args: Parameters<T>) => {
+    (args) => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
       }
 
       timeoutRef.current = setTimeout(() => {
-        callback(...args);
+        callback(args);
       }, delay);
     },
     [callback, delay],
